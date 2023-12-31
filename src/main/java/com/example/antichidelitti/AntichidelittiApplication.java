efde4f0474a5cbc79ce7dc76a59e29493c14f11b
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-12-31 22:35:13 +0100

    created project

---
+++ src/main/java/com/example/antichidelitti/AntichidelittiApplication.java
@@ -0,0 +1,13 @@
+package com.example.antichidelitti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntichidelittiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntichidelittiApplication.class, args);
+	}
+
+}
